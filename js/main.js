503ea71281a0056a6c68600a08b8df90e38d0363
mcweeney <mcweeney@seattleu.edu>
2016-08-29 15:31:40 -0400

    last finishing touches

--- js/main.js
+++ js/main.js
@@ -17,27 +17,36 @@ $(document).ready(function(){
 }
 // --------------------------- 
 			$.get(
+				// get google developers youtube data api
 			"https://www.googleapis.com/youtube/v3/playlistItems", {
+				// gather information you want from the data api. 
+				//For example, in a playlist resource, the snippet property contains properties like author, title, description, tags, and timeCreated
 				part: 'snippet',
+				// limit videos shown to 8
 				maxResults: 8, 
+				// ID of the walking dead playlist found off of AMC's youtube channel.
+				//reference http://support.andromo.com/kb/common-questions/getting-youtube-ids
 				playlistId: 'PLC7EC9FB2E211A261',
+				// my google developers key
 				key:'AIzaSyCgG-caHE-r6Q4Mh-XvB0H-ceZG1jZHnXI'},
 				function(data){
 					$.each(data.items, function(i, item){
+						// create output variable to contain what we want to place in our related videos div
 						var output;
 						console.log(item);
 						videoTitle = item.snippet.title;
 						videoId = item.snippet.resourceId.videoId;
-						
+						// source created to retrieve the videos from the playlist id
 						output = '<li><iframe src=\"//www.youtube.com/embed/'+videoId+'\"></iframe></li>';
 						
-						// append results into my div in my html document
+						// append results into my related-videos div in my html document. 
 						$('#related-videos').append(output);
 						
 					});
 				}
 	);
 		
+	// validate function for subscribe section at the bottom of the page.
 	$('#subscribe-btn').validate({
         submitHandler: function(form) {
             // If form is valid, submit it!
